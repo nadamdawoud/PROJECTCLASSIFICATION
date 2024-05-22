e7a7406ceaee064e8fbf7a44887b57221ef75374
vialete <114756482+vialete@users.noreply.github.com>
2024-05-22 13:28:24 +0200

    pls work

--- GUI/gui.py
+++ GUI/gui.py
@@ -2,14 +2,6 @@ import tkinter as tk
 from starting_page import StartingPage
 from hello_application import HelloApplication
 
-
-import pickle
-
-with open('models.pkl', 'rb') as f:
-    models = pickle.load(f)
-
-    print(models)
-
 class MainApplication(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -18,24 +10,22 @@ class MainApplication(tk.Tk):
         self.geometry("400x600")
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
-        
+
         self.frames = {}
         for F in (StartingPage, HelloApplication):
             page = F.__name__
-            frame = F(framebef=self, controller=self)
+            frame = F(parent=self, controller=self)
             self.frames[page] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_start_menu("StartingPage")
+        self.show_frame("StartingPage")
 
-    def show_start_menu(self, page):
-        frame = self.frames[page]
+    def show_frame(self, page_name, method=None, classifier=None):
+        frame = self.frames[page_name]
+        if page_name == "HelloApplication":
+            frame.set_method_and_classifier(method, classifier)
         frame.tkraise()
 
-    def show_main_menu(self, page, technique, classifier):
-        frame = self.frames[page]
-        frame.tkraise()
-        
 if __name__ == "__main__":
     app = MainApplication()
     app.mainloop()
