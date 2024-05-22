6eae9c1753a94be5a110addbb0ea8a5133e1ac23
vialete <114756482+vialete@users.noreply.github.com>
2024-05-22 13:30:52 +0200

    Revert "pls work"
    This reverts commit e7a7406ceaee064e8fbf7a44887b57221ef75374.

--- GUI/hello_application.py
+++ GUI/hello_application.py
@@ -1,76 +1,59 @@
 import tkinter as tk
 from tkinter import messagebox
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-import pandas as pd
-import os
 
 class HelloApplication(tk.Frame):
-    def __init__(self, parent, controller):
-        super().__init__(parent)
+    def __init__(self, framebef, controller):
+        super().__init__(framebef)
         self.controller = controller
-        self.method = None
-        self.classifier = None
 
         self.grid(row=0, column=0, sticky="nsew")
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
         
-        frame = tk.Frame(self)
-        frame.grid(row=0, column=0)
-        frame.grid_columnconfigure(0, weight=1)
-        frame.grid_rowconfigure([0, 1, 2, 3, 4, 5], weight=1)
+        # centering frame
+        framecent = tk.Frame(self)
+        framecent.grid(row=0, column=0)
+        framecent.grid_columnconfigure(0, weight=1)
+        framecent.grid_rowconfigure([0, 1, 2, 3, 4, 5], weight=1)
 
-        labelemail = tk.Label(frame, text="Email Input:", font=("Arial", 24))
+        # email input
+        labelemail = tk.Label(framecent, text="Email Input:", font=("Arial", 24))
         labelemail.grid(row=0, column=0, pady=10, padx=20, sticky="ew")
         
-        self.textemail = tk.Text(frame, height=15, font=("Arial", 16), wrap="word")
+        # email text space
+        self.textemail = tk.Text(framecent, height=15, font=("Arial", 16), wrap="word")
         self.textemail.grid(row=1, column=0, pady=10, padx=20, sticky="ew")
         self.textemail.insert(tk.END, "Write your email here..")
+        self.textemail.bind("<FocusIn>", self.clearholder)
         
-        checkbuton = tk.Button(frame, text="Check Input Email", bg="#6b6b6b", fg="white", font=("Arial", 16), width=20, height=2, command=self.on_check_input_email)
+        # check input button
+        checkbuton = tk.Button(framecent, text="Check Input Email", bg="#6b6b6b", fg="white", font=("Arial", 16),
+                               width=20, height=2, command=self.checkinputemailclick)
         checkbuton.grid(row=2, column=0, pady=10, padx=20, sticky="ew")
         
-        blackspace = tk.Frame(frame, height=2, bd=1, relief=tk.SUNKEN)
+        # separator
+        blackspace = tk.Frame(framecent, height=2, bd=1, relief=tk.SUNKEN)
         blackspace.grid(row=3, column=0, sticky="ew", padx=5, pady=20)
-    
-    def set_method_and_classifier(self, method, classifier):
-        self.method = method
-        self.classifier = classifier
-    
-    def on_check_input_email(self):
-        content = self.textemail.get("1.0", tk.END).strip()
+        
+        # ham Button
+        hambutton = tk.Button(framecent, text="Ham", bg="#8BC34A", fg="white", font=("Arial", 16), width=20, height=2, command=self.hambuttonclick)
+        hambutton.grid(row=4, column=0, pady=10, padx=20, sticky="ew")
+        
+        # spam Button
+        spambutton = tk.Button(framecent, text="Spam", bg="#F44336", fg="white", font=("Arial", 16), width=20, height=2, command=self.spambuttonclick)
+        spambutton.grid(row=5, column=0, pady=10, padx=20, sticky="ew")
+        
+    #to clear the text when user clicks
+    def clearholder(self, event):
+        if self.textemail.get("1.0", tk.END).strip() == "Write your email here..":
+            self.textemail.delete("1.0", tk.END)
 
-        preprocess = pd.read_csv('preprocessed.csv')
+    def checkinputemailclick(self):
+        contentofemail = self.textemail.get("1.0", tk.END).strip()
+        print("clicked")    
 
-        preprocess = preprocess.dropna(subset=['clean'])
+    def hambuttonclick(self):
+        print("clicked")     
 
-        if self.method == "Bag of Words":
-            vectorizer = CountVectorizer()
-        else:
-            vectorizer = TfidfVectorizer()
-
-        X = vectorizer.fit_transform(preprocess['clean'])
-        y = preprocess['label']
-
-        if self.classifier == "Naive Bayes":
-            classmodel = MultinomialNB()
-        elif self.classifier == "SVM":
-            classmodel = SVC()
-        elif self.classifier == "Logistic Regression":
-            classmodel = LogisticRegression()
-        elif self.classifier == "Random Forest":
-            classmodel = RandomForestClassifier()
-        elif self.classifier == "KNN":
-            classmodel = KNeighborsClassifier()
-
-        classmodel.fit(X, y)
-
-        vectorizedemail = vectorizer.transform([content])
-        prediction = classmodel.predict(vectorizedemail)
-        final = "Spam" if prediction[0] == 1 else "Not Spam"
-        messagebox.showinfo("Email Classification Result", final)
+    def spambuttonclick(self):
+        print("clicked")  
--- GUI/starting_page.py
+++ GUI/starting_page.py
@@ -2,39 +2,43 @@ import tkinter as tk
 from tkinter import ttk
 
 class StartingPage(tk.Frame):
-    def __init__(self, parent, controller):
-        super().__init__(parent)
+    def __init__(self, framebef, controller):
+        super().__init__(framebef)
         self.controller = controller
 
         self.grid(row=0, column=0, sticky="nsew")
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
         
-        frame = tk.Frame(self)
-        frame.grid(row=0, column=0)
-        frame.grid_columnconfigure(0, weight=1)
-        frame.grid_rowconfigure([0, 1, 2, 3, 4], weight=1)
+        framecent = tk.Frame(self)
+        framecent.grid(row=0, column=0)
+        framecent.grid_columnconfigure(0, weight=1)
+        framecent.grid_rowconfigure([0, 1, 2, 3, 4], weight=1)
 
-        self.label1 = tk.Label(frame, text="Choose Method:", font=("Arial", 16))
+        # dropdown for tfidf-bagofwords
+        self.label1 = tk.Label(framecent, text="Choose Pre-Process Technique:", font=("Arial", 16))
         self.label1.grid(row=0, column=0, pady=10, padx=20, sticky="ew")
         
-        self.metvar = tk.StringVar()
-        self.drop1 = ttk.Combobox(frame, textvariable=self.metvar, values=["Bag of Words", "TF-IDF"], font=("Arial", 16))
-        self.drop1.grid(row=1, column=0, pady=10, padx=20, sticky="ew")
-        self.drop1.set("Bag of Words")
+        self.method = tk.StringVar()
+        self.dropdown = ttk.Combobox(framecent, textvariable=self.method, values=["Bag of Words", "TF-IDF"], font=("Arial", 16), state="readonly")
+        self.dropdown.grid(row=1, column=0, pady=10, padx=20, sticky="ew")
+        self.dropdown.set("Bag of Words")
         
-        self.label2 = tk.Label(frame, text="Choose Classifier:", font=("Arial", 16))
+        # dropdown for methods 
+        # NOTE : CHANGE THE NAMES OF BUTTONS
+        self.label2 = tk.Label(framecent, text="Choose Classifier:", font=("Arial", 16),)
         self.label2.grid(row=2, column=0, pady=10, padx=20, sticky="ew")
         
-        self.classvar = tk.StringVar()
-        self.drop2 = ttk.Combobox(frame, textvariable=self.classvar, values=["Naive Bayes", "SVM", "Logistic Regression", "Random Forest", "KNN"], font=("Arial", 16))
-        self.drop2.grid(row=3, column=0, pady=10, padx=20, sticky="ew")
-        self.drop2.set("Naive Bayes")
+        self.option = tk.StringVar()
+        self.dropdown = ttk.Combobox(framecent, textvariable=self.option, values=["Logistic Regression", "Naive Bayes"], font=("Arial", 16), state="readonly")
+        self.dropdown.grid(row=3, column=0, pady=10, padx=20, sticky="ew")
+        self.dropdown.set("Logistic Regression")
         
-        self.contbutton = tk.Button(frame, text="Continue", font=("Arial", 16), command=self.on_continue)
-        self.contbutton.grid(row=4, column=0, pady=20, padx=20, sticky="ew")
+        # continue button
+        self.continuebutton = tk.Button(framecent, text="Continue", font=("Arial", 16), command=self.on_continue)
+        self.continuebutton .grid(row=4, column=0, pady=20, padx=20, sticky="ew")
 
     def on_continue(self):
-        method = self.metvar.get()
-        classifier = self.classvar.get()
-        self.controller.show_frame("HelloApplication", method, classifier)
+        technique = self.method.get()  # Get pre-processing technique
+        classifier = self.option.get()  # Get classifier
+        self.controller.show_frame("HelloApplication", technique, classifier)
