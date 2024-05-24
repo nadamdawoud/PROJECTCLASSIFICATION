74fed838842adcb903852817dfba10a491ac58b2
vialete <114756482+vialete@users.noreply.github.com>
2024-05-24 12:55:09 +0200

    buttons are working now

--- GUI/hello_application.py
+++ GUI/hello_application.py
@@ -1,13 +1,12 @@
 import tkinter as tk
 from tkinter import messagebox
+import model_prediction as mp
 
 class HelloApplication(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
-        print(f"HelloApplication controller: {self.controller}")  # Debug print
 
-        
         self.grid(row=0, column=0, sticky="nsew")
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
@@ -31,17 +30,33 @@ class HelloApplication(tk.Frame):
         self.textemail.bind("<FocusIn>", self.clear_text)
         
         # Check Input Email Button
-        checkbuton = tk.Button(frame, text="Check Input Email", bg="#6b6b6b", fg="white", font=("Arial", 16),width=20, height=2, command=self.on_check_input_email)
+        checkbuton = tk.Button(frame, text="Check Input Email", bg="#6b6b6b", fg="white", font=("Arial", 16), width=20, height=2, command=self.on_check_input_email)
         checkbuton.grid(row=2, column=0, pady=10, padx=20, sticky="ew")
         
         # Separator
         blackspace = tk.Frame(frame, height=2, bd=1, relief=tk.SUNKEN)
         blackspace.grid(row=3, column=0, sticky="ew", padx=5, pady=20)
-        
+
     def clear_text(self, event):
         if self.textemail.get("1.0", tk.END).strip() == "Write your email here..":
             self.textemail.delete("1.0", tk.END)
         
     def on_check_input_email(self):
         contentofemail = self.textemail.get("1.0", tk.END).strip()
-        messagebox.showinfo("Email Content", "Clicked")
+        method = self.controller.method
+        classf = self.controller.classifier
+
+        # Select the appropriate model based on user input
+        if method == "Bag of Words":
+            if classf == "Logistic Regression":
+                predic = mp.predict_lrbow(contentofemail)
+            elif classf == "Naive Bayes":
+                predic = mp.predict_cnbbow(contentofemail)
+        elif method == "TF-IDF":
+            if classf == "Logistic Regression":
+                predic = mp.predict_lrtfidf(contentofemail)
+            elif classf == "Naive Bayes":
+                predic = mp.predict_cnbtfidf(contentofemail)
+        
+        result = "Spam" if predic == [1] else "Not Spam"
+        messagebox.showinfo("Prediction Result", result)
